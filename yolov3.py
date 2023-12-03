3e225cabd2bd25ca8e147003369617d116dba4cd
Thisuli Amanethmi <124507196+Thisuli-Amanethmi@users.noreply.github.com>
2023-12-03 17:33:54 +0530

    Initial commit

---
+++ yolov3.py
@@ -0,0 +1,8 @@
+# requirements
+
+import tensorflow as tf # tensorflow - deep learning
+import numpy as np # numpy - numerical computation
+from PIL import Image, ImageDraw, ImageFont # pillow library - image processing
+from IPython.display import display # to display images in the notebook
+from seaborn import color_palette # seaborn - to bounding boxes colours - visuallization
+import cv2 # opencv - computer vision
