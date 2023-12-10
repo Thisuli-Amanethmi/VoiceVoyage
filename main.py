525b8c0854426aca28e33782f03e20b59830dc17
SinaliRajapaksha <150217792+SinaliRajapaksha@users.noreply.github.com>
2023-12-11 01:43:42 +0530

    Create Metadata DataFrames

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#import pandas as pd
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Metadata DataFrames
+# 13 objects
+metadata1 = ({
+    'ID': [1,2,3,4,5,6,7,8,9,10,11,12,13],
+    'Label': ['Wall','Chair','Table','Sofa','TV','Door','Window','Bed','Cupboard','DressingTable','Commode','Sink','Fridge'],
+    'File_Path':['']
+})
+# Staircase
+metadata2 = ({
+    'ID': [14],
+    'Label': ['Staircase'],
+    'File_Path':['']
+})
+# Washing Machine
+metadata3 = ({
+    'ID': [15],
+    'Label': ['WashingMachine'],
+    'File_Path':['']
+})
+# Stove cooker
+metadata4 = ({
+    'ID': [16],
+    'Label': ['Cooker'],
+    'File_Path':['']
+})
+# Dustbin
+metadata5 = ({
+    'ID': [17],
+    'Label': ['Dustbin'],
+    'File_Path':['']
+})
+# Broom
+metadata6 = ({
+    'ID': [18],
+    'Label': ['Broom'],
+    'File_Path':['']
+})
+
+# Accessing metadata for the first image
+#first_image_metadata1 = metadata1[0]
+#first_image_metadata2 = metadata2[0]
+#first_image_metadata3 = metadata3[0]
+#first_image_metadata4 = metadata4[0]
+#first_image_metadata5 = metadata5[0]
+#first_image_metadata6 = metadata6[0]
+
+#print(first_image_metadata1)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Staircase
+#metadata2 = pd.DataFrame({
+    #'ID': [14],
+    #'Label': ['Staircase'],
+    #'File_Path':['']
+#})
+# Merge metadata based on the 'ID' column
+#combined_metadata = pd.merge(metadata1, metadata2, metadata3, metadata4, metadata5, metadata6, on='ID', how='outer')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
