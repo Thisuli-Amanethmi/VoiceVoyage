1cc2cd586a38a313ab822df78ec7a7b9d7f2b293
vihindi kotalawala <vihindik@gmail.com>
2024-04-06 14:01:24 +0530

    Updated navigation algorithm

--- ObjectDetection/yolo8_custom_live.py
+++ ObjectDetection/yolo8_custom_live.py
@@ -1,69 +1,83 @@
 import time
-
 from OntologyGraph import Database as DB, NavAlgo as NV
 import cv2
 from ultralytics import YOLO
 import random
 import pyttsx3
-import OntologyExtraction as OE
+from ObjectDetection import OntologyExtraction as OE
+
+
 # Initialize the text-to-speech engine
 
 engine = pyttsx3.init()
-Previous_object_list=[]
-Current_object_list=[]
-Spatial_relationship={}
-announcement=""
 
+<<<<<<< Updated upstream
 
 #Get spatial relationship with objects using an ontology
+=======
+# Lists to keep track of objects
+Previous_object_list = []
+Current_object_list = []
+Spatial_relationship = {}
+
+######################
+# Get spatial relationship with objects using an ontology
+>>>>>>> Stashed changes
 def Get_spatial_relationship_with_objects():
-    Spatial_relationship["Object1"]=[]
-    Spatial_relationship["relationship"]=[]
-    Spatial_relationship["Object2"]=[]
+    Spatial_relationship = {"Object1": [], "relationship": [], "Object2": []}
     relationships = ["oppositeOf", "adjacentTo", "near"]
 
-    for j in range(3):
-        objects, relation = OE.Spatial_relationships(relationships[j])
-        for i in range(len(objects)):
-            if i % 2 == 1:
-                Spatial_relationship["Object1"].append(objects[i])
-            elif i % 2 == 0:
-                Spatial_relationship["Object2"].append(objects[i])
-                Spatial_relationship["relationship"].append(relation)
+    for j in relationships:
+        objects, relation = OE.Spatial_relationships(j)
+        for i in range(len(objects) // 2):
+            Spatial_relationship["Object1"].append(objects[2 * i])
+            Spatial_relationship["Object2"].append(objects[2 * i + 1])
+            Spatial_relationship["relationship"].append(relation)
     return Spatial_relationship
+############
 
+<<<<<<< Updated upstream
 
 def Check_the_spacial_relationship_of_current_objects(Spatial_relationship,Current_objects):
     # Initialize a list to hold announcements
+=======
+def Check_the_spatial_relationship_of_current_objects(Spatial_relationship, Current_objects):
+>>>>>>> Stashed changes
     announcements = []
 
-    # Iterate through each current object to check its spatial relationship
     for current_object in Current_objects:
-        # Check if the current object is in the Object1 list
         if current_object in Spatial_relationship["Object1"]:
             Index = Spatial_relationship["Object1"].index(current_object)
             relation = Spatial_relationship["relationship"][Index]
             obj2 = Spatial_relationship["Object2"][Index]
+<<<<<<< Updated upstream
             announcement_for_spatial_relation = "The " + current_object + " is " + relation + " " + obj2
             announcements = announcements.append(announcement_for_spatial_relation)
         # Check if the current object is in the Object2 list
+=======
+            announcements.append(f"The {current_object} is {relation} {obj2}")
+>>>>>>> Stashed changes
         elif current_object in Spatial_relationship["Object2"]:
             Index = Spatial_relationship["Object2"].index(current_object)
             relation = Spatial_relationship["relationship"][Index]
             obj1 = Spatial_relationship["Object1"][Index]
+<<<<<<< Updated upstream
             announcement_for_spatial_relation = "The " + current_object + " is " + relation + " to " + obj1
             announcements = announcements.append(announcement_for_spatial_relation)
+=======
+            announcements.append(f"The {current_object} is {relation} to {obj1}")
+>>>>>>> Stashed changes
 
-    # Check if any announcements were added; if not, return a default message
-    if not announcements:
-        return " "
+    return ", ".join(announcements) if announcements else " "
 
-    return announcements
+<<<<<<< Updated upstream
 
-
+=======
+###################
+>>>>>>> Stashed changes
 def navigation_algorithm(object_list):
     direction_counts = {'left': 0, 'right': 0, 'mid': 0}
-    object_indices = {'left': None, 'right': None, 'mid': None}
+    object_indices = {'left': [], 'right': [], 'mid': []}
     direction_mapping = {
         'left': [NV.queryLeft, NV.queryFarLeft],
         'right': [NV.Right, NV.queryFarRight],
@@ -74,32 +88,32 @@ def navigation_algorithm(object_list):
         for direction, objects in direction_mapping.items():
             if obj in objects:
                 direction_counts[direction] += 1
-                if object_indices[direction] is None:
-                    object_indices[direction] = object_list.index(obj)
+                object_indices[direction].append(object_list.index(obj))
 
     max_direction = max(direction_counts, key=direction_counts.get)
-    if direction_counts[max_direction] > 0:
-        print("NavigationAlgo",max_direction,object_indices[max_direction])
-        return max_direction, object_indices[max_direction]
-    return 0, 0
+    return max_direction, object_indices[max_direction][0] if direction_counts[max_direction] > 0 else 0
+
 
 
 def check_for_turn_back(current_object_list, previous_object_list):
-    return len(current_object_list) >= 2 and len(previous_object_list) >= 2 and \
-           current_object_list[1] in previous_object_list[-2:]
+    return set(current_object_list[-2:]).intersection(set(previous_object_list[-2:]))
 
+<<<<<<< Updated upstream
 
+=======
+################################################
+>>>>>>> Stashed changes
 def navigation_algo_part2(current_object_list, previous_object_list):
+    if not current_object_list or not previous_object_list:
+        return "None part 2"
+
     current_orientation, current_object_index = navigation_algorithm(current_object_list)
-    previous_orientation, _ = navigation_algorithm(previous_object_list[-1:])
-    turn_back_detected = check_for_turn_back(current_object_list, previous_object_list)
+    previous_orientation, _ = navigation_algorithm(previous_object_list[-2:])
 
-    if current_orientation is None or previous_orientation is None:
-        return
-
-    if turn_back_detected:
-        orientation_switch = {'left': 'right', 'right': 'left'}
+    if check_for_turn_back(current_object_list, previous_object_list):
+        orientation_switch = {'left': 'right', 'right': 'left', 'mid': 'mid'}
         current_orientation = orientation_switch.get(current_orientation, current_orientation)
+<<<<<<< Updated upstream
         previous_orientation = orientation_switch.get(previous_orientation, previous_orientation)
     announcement = " "
 
@@ -108,10 +122,18 @@ def navigation_algo_part2(current_object_list, previous_object_list):
         announcement = " is in your " + side + "."
     elif current_orientation != previous_orientation:
         announcement = " is in your right."
+=======
+
+    announcement = f"There is a {current_object_list[current_object_index]} in your {current_orientation}." if current_object_index is not None else ""
+>>>>>>> Stashed changes
 
     return announcement
 
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 class_list = ["bed","bookshelf","chair","clothes-rack","coffee-table","commode","cupboard","door","dressing-table","lamp","oven","pantry-cupboards","refrigerator","shoe-rack","sink","sofa","staircase","stove","table","tv","wall-art","washing-machine","window"]
 
 
@@ -148,10 +170,19 @@ while True:
         print("Can't receive frame (stream end?). Exiting ...")
         break
 
+<<<<<<< Updated upstream
     # perform some slow operation
     time.sleep(2)
 
     cv2.imshow('frame', frame)
+=======
+    # peform some slow operation
+    #time.sleep(2)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+>>>>>>> Stashed changes
 
     # resize the frame | small frame optimise the run.
     #frame = cv2.resize(frame, (frame_wid, frame_hyt))
@@ -209,7 +240,7 @@ while True:
                     Previous_object_list.append(detected_classes)
             Spatial_relation_dict=Get_spatial_relationship_with_objects()
             print(Spatial_relation_dict)
-            Spatial_announcement= Check_the_spacial_relationship_of_current_objects(Spatial_relation_dict,Current_object_list)
+            Spatial_announcement= Check_the_spatial_relationship_of_current_objects(Spatial_relation_dict,Current_object_list)
             announcement=navigation_algo_part2(Current_object_list,Previous_object_list)
             if Spatial_announcement!=" ":
                 engine.say(Spatial_announcement+announcement)
@@ -222,14 +253,10 @@ while True:
     cv2.imshow('ObjectDetection', frame)
 
     # Terminate run when "Q" pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) == ord('q'):
         break
 
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
-
-
-
-
 
